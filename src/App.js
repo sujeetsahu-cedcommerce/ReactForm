86ee313869f5704bd082d57a3c58960fb0070dda
sujeetsahu-cedcommerce <sujeetsahu@cedcoss.com>
2022-07-19 11:21:00 +0530

    Create a text box that accepts only numbers and display the same using buttons

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './App.css';
+import Textbox from './Textbox';
+
+function App() {
+  return (
+    <div className="App">
+      <Textbox/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Textbox.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+
+export default class Textbox extends Component {
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+          text_values:"",
+          submitted_text_value:"",
+      }
+    }
+
+    GetTextValue = (e) => {
+        this.setState({
+            text_values:e.target.value,
+        })
+    }
+
+    SubmitForm = (event) => {
+        this.setState({
+            submitted_text_value:this.state.text_values,
+        })
+        event.preventDefault();
+    }
+  render() {
+    return (
+      <form onSubmit={this.SubmitForm}>
+          <input type="number" value={this.state.text_values} onChange={this.GetTextValue}></input>
+          <input type="submit" value="Submit" />
+          <p>{this.state.submitted_text_value}</p>
+      </form>
+    )
+  }
+}
